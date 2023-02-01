074cccbf2b9209b138aeb73a46ffa6aa042fc0d4
rousek26 <ada.rousek@gmail.com>
2023-02-01 12:06:44 +0100

    VLC fix

--- js/video.js
+++ js/video.js
@@ -1,6 +1,5 @@
 function openVideo(file){
     document.getElementById("videoPlayer").src = file;
-    moveWindow(5);
-    openWindow(5);
+    focusWindow('Video přehrávač')
     setTimeout(() => {window_z_index(5);}, 50);   
 }

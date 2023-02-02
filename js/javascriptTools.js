fc0713361c1734805eb75474bb057de661eadf1e
Komkry <Komkry@seznam.cz>
2023-02-02 12:08:00 +0100

    files extensions, file text random, new JSTOOLS functions

--- js/javascriptTools.js
+++ js/javascriptTools.js
@@ -7,3 +7,18 @@ function sleep (time) {
 // nebo pokud chcete použít aby se na tomto řádku čekalo a pak se provedl zbytek:
 // await sleep(1000);
 // ALE POZOR, await funguje jen uvnitř async funkce
+
+//generate rendom string with length
+function generateRandomString(length) {
+    let result = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}

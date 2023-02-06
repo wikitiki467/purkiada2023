05221d902188db363dc9b9320b5e028651325fba
Komkry <Komkry@seznam.cz>
2023-02-06 20:20:07 +0100

    pan backor slide transition

---
+++ js/forms.js
@@ -0,0 +1,36 @@
+document.querySelectorAll('.ntl-form').forEach(function (form) {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
+      let formData = new FormData(form)
+      fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "multipart/form-data" },
+        body: new URLSearchParams(formData).toString()
+      }).then(() => ntlFormSuccess()).catch((error) =>
+        alert(error))
+    });
+  
+    function ntlFormSuccess() {
+      console.log("form submitted")
+      form.reset();
+    }
+  });
+  
+  function validateForm() {
+      let errors = [];
+      // loop through all input fields
+      for (var i = 0; i < document.forms[0].length; i++) {
+        // if the field is empty
+        if (document.forms[0][i].value == "") {
+          // add the field name to the errors array
+          errors.push(document.forms[0][i].name);
+        }
+      }
+      // if there are errors
+      if (errors.length > 0) {
+          console.log("The following fields are required: " + errors.join(", "));
+          return false;
+      }
+      return true;
+  }
+  document.write("new version 4")

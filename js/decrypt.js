b5550832666e7e61533f6eaea9d9417d6e54732b
rousek26 <ada.rousek@gmail.com>
2023-02-09 22:55:44 +0100

    Update decrypt

--- js/decrypt.js
+++ js/decrypt.js
@@ -4,21 +4,49 @@ const secondCode = generateRandomStringOfRandomLength(6,9);
 const thirdCode = generateRandomStringOfRandomLength(6,9);
 const codes = [firstCode, secondCode, thirdCode];
 
-function decrypt(){
+
+function decrypt1(){
+    btn = document.getElementById("decryptBTN1")
     input = document.getElementById("decrypt1").value;
     if(input == getCode(0)){
         document.getElementById("decryptmsg").innerHTML = "Chyba - kód chyby " + errorCode;
         completeLevel(18);
-    }else if (input == getCode(1)){
-        completeLevel(20);
-        incrementCode(thirdCode);
-        document.getElementById("decryptmsg").innerHTML = "Chyba - nesprávný kód!";
-    }else if (input == getCode(2)){
-        completeLevel(23);
-    }else{
+        document.getElementById("divDecrypt2").style.display = "block";
+        document.getElementById("decrypt1").setAttribute("readonly", "")
+        btn.remove();
+    }
+    else{
         document.getElementById("decryptmsg").innerHTML = "Nedefinovaná funkce!";
     }
 }
+function decrypt2() {
+    btn = document.getElementById("decryptBTN2")
+    input = document.getElementById("decrypt2").value;
+    if (input == getCode(1)){
+        completeLevel(20);
+        document.getElementById("decryptmsg").innerHTML = "Chyba - nesprávný kód!";
+        document.getElementById("divDecrypt3").style.display = "block";
+        document.getElementById("decrypt2").setAttribute("readonly", "")
+        btn.remove();
+    }
+    else{
+        document.getElementById("decryptmsg").innerHTML = "Nedefinovaná funkce!";
+    }
+}
+
+function decrypt3() {
+    btn = document.getElementById("decryptBTN3")
+    input = document.getElementById("decrypt3").value;
+    if (input == getCode(2)){
+        completeLevel(23);  
+        document.getElementById("decrypt3").setAttribute("readonly", "")
+        btn.remove();
+    }
+    else{
+        document.getElementById("decryptmsg").innerHTML = "Nedefinovaná funkce!";
+    }
+}
+
 function getCode(number){
     return codes[number];
 }
